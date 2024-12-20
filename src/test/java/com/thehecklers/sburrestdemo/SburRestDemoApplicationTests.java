8c08cad210ce42a48826792552cc5b71de46bf7b
Admin <kwy0617@gmail.com>
2024-12-20 10:00:47 +0900

    gggg

---
+++ src/test/java/com/thehecklers/sburrestdemo/SburRestDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.thehecklers.sburrestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SburRestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
